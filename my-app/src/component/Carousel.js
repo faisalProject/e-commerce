9c5d745ca9d6aadbeb639fd9cc03bf646954a71b
faisalProject <mf494284@gmail.com>
2022-12-30 16:04:20 +0700

    partial update

--- my-app/src/component/Carousel.js
+++ my-app/src/component/Carousel.js
@@ -1,9 +1,19 @@
-import carouselImage from '../images/carousel-image.jpg'
+import carouselImage from '../images/carousel-image2.jpg'
 
 const Carousel = () => {
     return(
         <div className="container carousel-contents">
-            
+            <ul className="slider">
+                <li className="slide">
+                    <img src={carouselImage} alt="" />
+                </li>
+                <li className="slide">
+                    <img src={carouselImage} alt="" />
+                </li>
+                <li className="slide">
+                    <img src={carouselImage} alt="" />
+                </li>
+            </ul>
         </div>
     )
 }
