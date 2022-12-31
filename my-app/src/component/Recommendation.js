8b840d61abf5e6a7e674f9e4743ed38fe0c9fb42
faisalProject <mf494284@gmail.com>
2022-12-31 10:37:51 +0700

    partial update

--- my-app/src/component/Recommendation.js
+++ my-app/src/component/Recommendation.js
@@ -1,7 +1,271 @@
+import {Link} from 'react-router-dom';
+import shoes from '../images/shoes.jpg';
+
 const Recommendation = () => {
     return(
-        <div className="container recommendation">
-            <div className="contents"></div>
+        <div className="container recommendation-contents">
+            <div className="recommendation">
+                <div className="box">
+                    <h5>REKOMENDASI</h5>
+                </div>
+                <div className="line"></div>
+            </div>
+
+            <div className="product-container">
+                {/* looping for product */}
+                <div className="product">
+                    <img src={shoes} alt="" />
+                    <div className="desc">
+                        <Link to='/' className='title'>
+                            Sepatu Pria Causal Kulit Original Premium....
+                        </Link>
+                        <p className='price'>Rp 150.000</p>
+                        <div className='discount-container'>
+                            <div className='discount'>
+                                <p>73%</p>
+                            </div>
+                            <p className='price-old'>Rp 425.000</p>
+                        </div>
+                        <div className='rating'>
+                            <i className="bi bi-star-fill"></i>
+                            <div className='sold'>
+                                <p>4.7 |</p> <p>Terjual 100++</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="product">
+                    <img src={shoes} alt="" />
+                    <div className="desc">
+                        <Link to='/' className='title'>
+                            Sepatu Pria Causal Kulit Original Premium....
+                        </Link>
+                        <p className='price'>Rp 150.000</p>
+                        <div className='discount-container'>
+                            <div className='discount'>
+                                <p>73%</p>
+                            </div>
+                            <p className='price-old'>Rp 425.000</p>
+                        </div>
+                        <div className='rating'>
+                            <i className="bi bi-star-fill"></i>
+                            <div className='sold'>
+                                <p>4.7 |</p> <p>Terjual 100++</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="product">
+                    <img src={shoes} alt="" />
+                    <div className="desc">
+                        <Link to='/' className='title'>
+                            Sepatu Pria Causal Kulit Original Premium....
+                        </Link>
+                        <p className='price'>Rp 150.000</p>
+                        <div className='discount-container'>
+                            <div className='discount'>
+                                <p>73%</p>
+                            </div>
+                            <p className='price-old'>Rp 425.000</p>
+                        </div>
+                        <div className='rating'>
+                            <i className="bi bi-star-fill"></i>
+                            <div className='sold'>
+                                <p>4.7 |</p> <p>Terjual 100++</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="product">
+                    <img src={shoes} alt="" />
+                    <div className="desc">
+                        <Link to='/' className='title'>
+                            Sepatu Pria Causal Kulit Original Premium....
+                        </Link>
+                        <p className='price'>Rp 150.000</p>
+                        <div className='discount-container'>
+                            <div className='discount'>
+                                <p>73%</p>
+                            </div>
+                            <p className='price-old'>Rp 425.000</p>
+                        </div>
+                        <div className='rating'>
+                            <i className="bi bi-star-fill"></i>
+                            <div className='sold'>
+                                <p>4.7 |</p> <p>Terjual 100++</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="product">
+                    <img src={shoes} alt="" />
+                    <div className="desc">
+                        <Link to='/' className='title'>
+                            Sepatu Pria Causal Kulit Original Premium....
+                        </Link>
+                        <p className='price'>Rp 150.000</p>
+                        <div className='discount-container'>
+                            <div className='discount'>
+                                <p>73%</p>
+                            </div>
+                            <p className='price-old'>Rp 425.000</p>
+                        </div>
+                        <div className='rating'>
+                            <i className="bi bi-star-fill"></i>
+                            <div className='sold'>
+                                <p>4.7 |</p> <p>Terjual 100++</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="product">
+                    <img src={shoes} alt="" />
+                    <div className="desc">
+                        <Link to='/' className='title'>
+                            Sepatu Pria Causal Kulit Original Premium....
+                        </Link>
+                        <p className='price'>Rp 150.000</p>
+                        <div className='discount-container'>
+                            <div className='discount'>
+                                <p>73%</p>
+                            </div>
+                            <p className='price-old'>Rp 425.000</p>
+                        </div>
+                        <div className='rating'>
+                            <i className="bi bi-star-fill"></i>
+                            <div className='sold'>
+                                <p>4.7 |</p> <p>Terjual 100++</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="product">
+                    <img src={shoes} alt="" />
+                    <div className="desc">
+                        <Link to='/' className='title'>
+                            Sepatu Pria Causal Kulit Original Premium....
+                        </Link>
+                        <p className='price'>Rp 150.000</p>
+                        <div className='discount-container'>
+                            <div className='discount'>
+                                <p>73%</p>
+                            </div>
+                            <p className='price-old'>Rp 425.000</p>
+                        </div>
+                        <div className='rating'>
+                            <i className="bi bi-star-fill"></i>
+                            <div className='sold'>
+                                <p>4.7 |</p> <p>Terjual 100++</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="product">
+                    <img src={shoes} alt="" />
+                    <div className="desc">
+                        <Link to='/' className='title'>
+                            Sepatu Pria Causal Kulit Original Premium....
+                        </Link>
+                        <p className='price'>Rp 150.000</p>
+                        <div className='discount-container'>
+                            <div className='discount'>
+                                <p>73%</p>
+                            </div>
+                            <p className='price-old'>Rp 425.000</p>
+                        </div>
+                        <div className='rating'>
+                            <i className="bi bi-star-fill"></i>
+                            <div className='sold'>
+                                <p>4.7 |</p> <p>Terjual 100++</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="product">
+                    <img src={shoes} alt="" />
+                    <div className="desc">
+                        <Link to='/' className='title'>
+                            Sepatu Pria Causal Kulit Original Premium....
+                        </Link>
+                        <p className='price'>Rp 150.000</p>
+                        <div className='discount-container'>
+                            <div className='discount'>
+                                <p>73%</p>
+                            </div>
+                            <p className='price-old'>Rp 425.000</p>
+                        </div>
+                        <div className='rating'>
+                            <i className="bi bi-star-fill"></i>
+                            <div className='sold'>
+                                <p>4.7 |</p> <p>Terjual 100++</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="product">
+                    <img src={shoes} alt="" />
+                    <div className="desc">
+                        <Link to='/' className='title'>
+                            Sepatu Pria Causal Kulit Original Premium....
+                        </Link>
+                        <p className='price'>Rp 150.000</p>
+                        <div className='discount-container'>
+                            <div className='discount'>
+                                <p>73%</p>
+                            </div>
+                            <p className='price-old'>Rp 425.000</p>
+                        </div>
+                        <div className='rating'>
+                            <i className="bi bi-star-fill"></i>
+                            <div className='sold'>
+                                <p>4.7 |</p> <p>Terjual 100++</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="product">
+                    <img src={shoes} alt="" />
+                    <div className="desc">
+                        <Link to='/' className='title'>
+                            Sepatu Pria Causal Kulit Original Premium....
+                        </Link>
+                        <p className='price'>Rp 150.000</p>
+                        <div className='discount-container'>
+                            <div className='discount'>
+                                <p>73%</p>
+                            </div>
+                            <p className='price-old'>Rp 425.000</p>
+                        </div>
+                        <div className='rating'>
+                            <i className="bi bi-star-fill"></i>
+                            <div className='sold'>
+                                <p>4.7 |</p> <p>Terjual 100++</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="product">
+                    <img src={shoes} alt="" />
+                    <div className="desc">
+                        <Link to='/' className='title'>
+                            Sepatu Pria Causal Kulit Original Premium....
+                        </Link>
+                        <p className='price'>Rp 150.000</p>
+                        <div className='discount-container'>
+                            <div className='discount'>
+                                <p>73%</p>
+                            </div>
+                            <p className='price-old'>Rp 425.000</p>
+                        </div>
+                        <div className='rating'>
+                            <i className="bi bi-star-fill"></i>
+                            <div className='sold'>
+                                <p>4.7 |</p> <p>Terjual 100++</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
