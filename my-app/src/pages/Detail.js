4f846d6ea3e01078cfb5f0f230389b6b8ba7f2b7
faisalProject <mf494284@gmail.com>
2023-01-02 23:13:05 +0700

    added detail page

--- my-app/src/pages/Detail.js
+++ my-app/src/pages/Detail.js
@@ -1,7 +1,13 @@
 const Detail = () => {
     return(
         <div className="product-detail-contents">
-            <div className="container"></div>
+            <div className="container">
+                <div className="left"></div>
+                <div className="right"></div>
+            </div>
+            <div className="container">
+                <div className="box"></div>
+            </div>
         </div>
     )
 }
